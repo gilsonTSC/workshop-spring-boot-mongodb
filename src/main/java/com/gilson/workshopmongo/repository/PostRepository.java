e38500000cb0fa6b09ae78e4918f13d90ab6da3a
Gilson Trajano <gilson.costa@dce.ufpb.br>
2020-02-19 20:54:09 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/gilson/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.gilson.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gilson.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
